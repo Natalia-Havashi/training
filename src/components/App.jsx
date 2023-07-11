684e971e852bd52ccae1780d4821682f2ac3e9d7
Natalia-Havashi <natali.gavashi@gmail.com>
2023-07-11 18:04:08 +0300

    training

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from "react";
+import Button from "../Button/Button";
+import { fetchMovies } from "services/moviesApi";
+
+
+class App extends Component  {
+  state ={ 
+    isListShown: false,
+    movies: [],
+    isLoader: false,
+  }
+  componentDidUpdate(prevState,prevProps) {
+if(prevState.isListShown !== this.state.isListShown && this.state.isListShown){
+  this.setState({isLoader:true});
+  fetchMovies().then(res => {
+    
+  })
+}
+
+  }
+
+  togelVisibility = () => {
+this.setState(prevState => {
+  return {isListShown: !prevState.isListShown}
+})
+  }
+  render() {
+    const {isListShown} = this.state;
+    return (
+  
+     <Button 
+     text={isListShown ? "Hide movies" : "Show movies"}
+     clickHandler={this.togelVisibility}
+     />
+ 
   );
+  }
+  
 };
+
+export default App
